4e0bd73449862116d49c94042f7108344a8681dd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-06-27 23:41:29 +0200

    Add a gsettings script for Ubuntu to remove some defaults

---
+++ gnome/.dependencies/include/ubuntu/gsettings.sh
@@ -0,0 +1,13 @@
+# Disable the default Ubuntu shell extension that cobbles Super + Number
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+
+# Disable Ubuntu default keybindings that cobble Super + Number
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
