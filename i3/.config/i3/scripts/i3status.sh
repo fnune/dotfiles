97411a1617a58dd681369baf74d3668dd020c0aa
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2021-09-15 15:15:42 +0200

    Use a nicer indicator for do-not-disturb mode

--- i3/.config/i3/scripts/i3status.sh
+++ i3/.config/i3/scripts/i3status.sh
@@ -2,7 +2,7 @@
 i3status | while :
 do
   DND_PAUSED=$(dunstctl is-paused)
-  if [ "$DND_PAUSED" = "true" ]; then DND_INDICATOR="[DO NOT DISTURB]"; else DND_INDICATOR=""; fi
+  if [ "$DND_PAUSED" = "true" ]; then DND_INDICATOR="🔕 "; else DND_INDICATOR=""; fi
   read default_status
   echo "$DND_INDICATOR$default_status" || exit 1
 done
