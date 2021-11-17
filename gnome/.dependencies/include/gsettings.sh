81a2f96efe3fab13d009a338699bbacd4de11635
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2021-11-17 09:46:00 +0100

    Update gsettings.sh script with new keybindings/removals

--- gnome/.dependencies/include/gsettings.sh
+++ gnome/.dependencies/include/gsettings.sh
@@ -26,12 +26,21 @@ done
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
 
+# Disable Gnome default keybindings that cobble Super + Number
+for application in {1..9}
+do
+  gsettings set org.gnome.shell.keybindings switch-to-application-$application "[]"
+done
+
 # Switching to workspaces and moving windows to workspaces workspace numbers
-for workspace in {1..10}
+for workspace in {1..9}
 do
   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$workspace "['<Super>$workspace']"
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$workspace "['<Super><Shift>$workspace']"
 done
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
 
 # Maximizing and full-screen
 gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>f']"
@@ -64,3 +73,7 @@ gsettings set org.gnome.desktop.peripherals.keyboard delay 200
 
 # A text scaling factor that works great on my monitor
 gsettings set org.gnome.desktop.interface text-scaling-factor 1
+
+# Stop the Super key from opening the overview and choose another shortcut
+gsettings set org.gnome.mutter overlay-key ""
+gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>d']"
