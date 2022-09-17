c818941b9bcd0e4dad0d826c6de646bd225e0b5b
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-09-17 15:10:25 +0200

    Evolve vscode config a little

--- code/.config/vscode-neovim/init.lua
+++ code/.config/vscode-neovim/init.lua
@@ -1,9 +1,9 @@
 vim.cmd 'packadd paq-nvim'
 
 require 'paq' {
-  { "savq/paq-nvim", opt = true },
   { "AndrewRadev/tagalong.vim" },
   { "editorconfig/editorconfig-vim" },
+  { "savq/paq-nvim", opt = true },
   { "tpope/vim-abolish" },
   { "tpope/vim-repeat" },
   { "tpope/vim-surround" },
@@ -23,6 +23,10 @@ vim.cmd("nmap gcc <Plug>VSCodeCommentaryLine")
 
 vim.cmd("nnoremap 0 ^")
 vim.cmd("nnoremap <leader>l :nohl<CR>")
+vim.cmd("nnoremap j gj")
+vim.cmd("nnoremap k gk")
+vim.cmd("nnoremap <c-u> <c-u>zz")
+vim.cmd("nnoremap <c-d> <c-d>zz")
 
 local function vscmap(mapping, action)
   vim.cmd(mapping .. " <Cmd>call VSCodeNotify('" .. action .. "')<CR>")
@@ -32,6 +36,14 @@ vscmap("nnoremap <leader>f", "workbench.action.quickOpen")
 vscmap("nnoremap <leader>F", "workbench.view.search")
 vscmap("nnoremap <leader>e", "workbench.view.explorer")
 vscmap("nnoremap <leader>p", "editor.action.formatDocument")
-vscmap("nnoremap <leader>a", "editor.action.codeAction")
+vscmap("nnoremap <leader>a", "editor.action.sourceAction")
 vscmap("nnoremap <leader>j", "editor.action.marker.nextInFiles")
 vscmap("nnoremap <leader>k", "editor.action.marker.prevInFiles")
+vscmap("nnoremap <leader>r", "editor.action.rename")
+vscmap("nnoremap <s-h>", "workbench.action.previousEditor")
+vscmap("nnoremap <s-l>", "workbench.action.nextEditor")
+
+vim.cmd("nnoremap <m-k> <Cmd>call VSCodeCallVisual('editor.action.moveLinesUpAction',1)<CR>")
+vim.cmd("nnoremap <m-j> <Cmd>call VSCodeCallVisual('editor.action.moveLinesDownAction',1)<CR>")
+vim.cmd("xnoremap <m-k> <Cmd>call VSCodeCallVisual('editor.action.moveLinesUpAction',1)<CR>")
+vim.cmd("xnoremap <m-j> <Cmd>call VSCodeCallVisual('editor.action.moveLinesDownAction',1)<CR>")
