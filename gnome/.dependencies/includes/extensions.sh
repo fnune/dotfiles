b839c7b02d8baaee30bb27e0194584f6c0e59cc0
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-11-26 13:37:09 +0100

    Install sound output device chooser in extensions.sh

--- gnome/.dependencies/includes/extensions.sh
+++ gnome/.dependencies/includes/extensions.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+sudo apt install -y wget curl
+
 wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
 chmod +x gnome-shell-extension-installer
 sudo mv gnome-shell-extension-installer /usr/bin/
@@ -12,3 +14,4 @@ gnome-shell-extension-installer 517  3.38  # https://extensions.gnome.org/extens
 gnome-shell-extension-installer 5278 --yes # https://extensions.gnome.org/extension/5278/pano/
 gnome-shell-extension-installer 615  3.38  # https://extensions.gnome.org/extension/615/appindicator-support/
 gnome-shell-extension-installer 959  3.38  # https://extensions.gnome.org/extension/959/disable-workspace-switcher-popup/
+gnome-shell-extension-installer 906  3.38  # https://extensions.gnome.org/extension/906/sound-output-device-chooser/
