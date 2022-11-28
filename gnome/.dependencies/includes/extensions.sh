b458c4b852e33b4140289ee36ced4c51cffa5c92
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-11-28 23:45:38 +0100

    Disable installing pano in extensions.sh

--- gnome/.dependencies/includes/extensions.sh
+++ gnome/.dependencies/includes/extensions.sh
@@ -11,7 +11,9 @@ sudo mv gnome-shell-extension-installer /usr/bin/
 gnome-shell-extension-installer 2141 3.38  # https://extensions.gnome.org/extension/2141/horizontal-workspaces/
 gnome-shell-extension-installer 3851 3.38  # https://extensions.gnome.org/extension/3851/workspaces-bar/
 gnome-shell-extension-installer 517  3.38  # https://extensions.gnome.org/extension/517/caffeine/
-gnome-shell-extension-installer 5278 --yes # https://extensions.gnome.org/extension/5278/pano/
 gnome-shell-extension-installer 615  3.38  # https://extensions.gnome.org/extension/615/appindicator-support/
 gnome-shell-extension-installer 959  3.38  # https://extensions.gnome.org/extension/959/disable-workspace-switcher-popup/
 gnome-shell-extension-installer 906  3.38  # https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+
+# Pano curently doesn't work on 3.38. Wait for Debian to be on GNOME 40+
+# gnome-shell-extension-installer 5278 --yes # https://extensions.gnome.org/extension/5278/pano/
