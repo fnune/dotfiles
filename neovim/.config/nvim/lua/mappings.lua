8954c439d1bf9307ac14e9840da4355aeda34b4c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-01-29 18:52:53 +0100

    Fix buffer wipeout command

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -48,7 +48,7 @@ nmap('<M-0>', '<cmd>BufferLast<cr>')
 
 nmap('<leader>q', ':close<cr>')
 nmap('<leader>bq', '<cmd>BufferClose<cr>')
-nmap('<leader>bQ', '<cmd>BufferWipeout<cr>')
+nmap('<leader>bQ', '<cmd>BufferCloseAllButCurrent<cr> <cmd>BufferClose<cr>')
 nmap('<leader>be', '<cmd>BufferCloseAllButCurrent<cr>')
 
 nmap('<leader>bb', '<cmd>BufferOrderByBufferNumber<cr>')
