0f66d5b6008cc24a04ab3f97b17ca439ab49cde1
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-02-12 12:37:27 +0100

    Upgrade to debian 12 and change related GNOME settings

--- gnome/.dependencies/includes/extensions.sh
+++ gnome/.dependencies/includes/extensions.sh
@@ -8,12 +8,10 @@ wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell
 chmod +x gnome-shell-extension-installer
 sudo mv gnome-shell-extension-installer /usr/bin/
 
-gnome-shell-extension-installer 2141 3.38  # https://extensions.gnome.org/extension/2141/horizontal-workspaces/
-gnome-shell-extension-installer 3851 3.38  # https://extensions.gnome.org/extension/3851/workspaces-bar/
-gnome-shell-extension-installer 517  3.38  # https://extensions.gnome.org/extension/517/caffeine/
-gnome-shell-extension-installer 615  3.38  # https://extensions.gnome.org/extension/615/appindicator-support/
-gnome-shell-extension-installer 959  3.38  # https://extensions.gnome.org/extension/959/disable-workspace-switcher-popup/
-gnome-shell-extension-installer 906  3.38  # https://extensions.gnome.org/extension/906/sound-output-device-chooser/
-
-# Pano curently doesn't work on 3.38. Wait for Debian to be on GNOME 40+
-# gnome-shell-extension-installer 5278 --yes # https://extensions.gnome.org/extension/5278/pano/
+gnome-shell-extension-installer 2141 --yes # https://extensions.gnome.org/extension/2141/horizontal-workspaces/
+gnome-shell-extension-installer 3851 --yes # https://extensions.gnome.org/extension/3851/workspaces-bar/
+gnome-shell-extension-installer 4099 --yes # https://extensions.gnome.org/extension/4099/no-overview/
+gnome-shell-extension-installer 4980 --yes # https://extensions.gnome.org/extension/4980/disable-workspace-switcher/
+gnome-shell-extension-installer 517  --yes # https://extensions.gnome.org/extension/517/caffeine/
+gnome-shell-extension-installer 5278 --yes # https://extensions.gnome.org/extension/5278/pano/
+gnome-shell-extension-installer 615  --yes # https://extensions.gnome.org/extension/615/appindicator-support/
