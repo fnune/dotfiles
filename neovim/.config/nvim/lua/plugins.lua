44633199898984badf0abef6bf734003bafbccdd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-02-12 12:48:32 +0100

    Tint neotree when it's not active

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -13,6 +13,7 @@ vim.call('plug#begin')
 Plug('APZelos/blamer.nvim')
 Plug('AndrewRadev/tagalong.vim')
 Plug('Raimondi/delimitMate')
+Plug('levouh/tint.nvim')
 Plug('lewis6991/gitsigns.nvim')
 Plug('christoomey/vim-tmux-navigator')
 Plug('editorconfig/editorconfig-vim')
@@ -151,3 +152,19 @@ require('diffview').setup {
 }
 
 require('gitsigns').setup {}
+
+require("tint").setup({
+    tint = -10,
+    tint_background_colors = true,
+    saturation = 0.8,
+    window_ignore_function = function(winid)
+      local bufid = vim.api.nvim_win_get_buf(winid)
+      local bufname = vim.api.nvim_buf_get_name(bufid)
+
+      if string.find(bufname, "NvimTree_1") then
+        return false
+      end
+
+      return true
+    end
+})
