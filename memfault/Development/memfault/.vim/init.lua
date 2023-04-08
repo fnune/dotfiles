e0dfa13a80267ef6b5021b374cf0e199d4087d83
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-04-08 16:40:42 +0200

    Add a fairly crude dap config for chromium

--- memfault/Development/memfault/.vim/init.lua
+++ memfault/Development/memfault/.vim/init.lua
@@ -1,4 +1,28 @@
+local dap = require("dap")
 local dap_python = require('dap-python')
 
 dap_python.setup('~/.mambaforge/envs/memfault/bin/python')
 dap_python.test_runner = 'pytest'
+
+dap.adapters.chrome = {
+  type = "executable",
+  command = "node",
+  -- Grab this from https://github.com/Microsoft/vscode-chrome-debug and
+  -- build it with npm install && npm run build
+  args = { os.getenv("HOME") .. "/Development/vscode-chrome-debug/out/src/chromeDebug.js" }
+}
+
+-- To attach, launch Chromium with:
+-- flatpak run org.chromium.Chromium --remote-debugging-port=9222
+dap.configurations.typescriptreact = {
+  {
+    type = "chrome",
+    request = "attach",
+    program = "${file}",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+    port = 9222,
+    webRoot = "${workspaceFolder}"
+  }
+}
