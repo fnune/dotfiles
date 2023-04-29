68374b3fff66f03528e32a356ba8280cfd581420
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-04-29 17:17:31 +0200

    Search in hidden files Telescope live_grep, too

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,13 @@ require('telescope').setup {
   defaults = {
     file_ignore_patterns = { ".git/*", ".cache/*" },
   },
+  pickers = {
+    find_files = { hidden = true },
+    live_grep = { additional_args = { "--hidden" } },
+  },
 }
 
 require('telescope').load_extension('fzf')
 
-nmap('<leader>f', ':Telescope find_files hidden=true<CR>')
+nmap('<leader>f', ':Telescope find_files<CR>')
 nmap('<leader>F', ':Telescope live_grep<CR>')
