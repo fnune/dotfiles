36fc4fd3bf58f5f5f3ef8a52ce0f0e84692e3c59
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-04-30 17:20:16 +0200

    Finally add a command to reload my config (<leader>R)

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,22 @@
-require("system")
-require("settings")
-require("mappings")
-require("plugins")
-require("theme")
-require("statusline")
+local modules = {
+  "system",
+  "settings",
+  "mappings",
+  "plugins",
+  "theme",
+  "statusline",
+}
+
+for _, module in pairs(modules) do
+  require(module)
+end
+
+vim.keymap.set("n", "<leader>R", function()
+  for _, module in pairs(modules) do
+    package.loaded[module] = nil
+  end
+
+  dofile(vim.env.MYVIMRC)
+
+  vim.cmd("echo '↻ Reloaded " .. vim.env.MYVIMRC .. "'")
+end)
