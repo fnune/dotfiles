7a2c7a7bc89503a26b03a6c7a689ae6708b4bab7
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-02 08:59:07 +0200

    Add g:coc_status to lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -13,5 +13,14 @@ lualine.setup({
     globalstatus = true,
     section_separators = { "", "" },
     theme = theme,
-  }
+  },
+  -- See defaults: https://github.com/nvim-lualine/lualine.nvim#default-configuration
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'g:coc_status', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
 })
