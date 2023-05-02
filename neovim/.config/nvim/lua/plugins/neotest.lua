c5da0a34b96d8ae9050a36b81cbc9ed1e13c2a0c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-02 23:31:44 +0200

    Fix neotest keybindings I broke with the move to mapx

--- neovim/.config/nvim/lua/plugins/neotest.lua
+++ neovim/.config/nvim/lua/plugins/neotest.lua
@@ -2,10 +2,11 @@ local m = require("mapx")
 local neotest = require("neotest")
 
 m.nname("<leader>t", "Tests")
-m.nmap("<leader>ts", ":lua require('neotest').run.run()<cr>", "Run the closest test")
-m.nmap("<leader>ta", ":lua require('neotest').run.run(vim.fn.expand(' % '))<cr>", "Run all tests")
-m.nmap("<leader>ds", ":lua require('neotest').run.run({ strategy = 'dap' })<cr>", "Debug the closest test")
-m.nmap("<leader>T", ":lua require('neotest').summary.toggle()<cr>", "Toggle the test summary view")
+m.nmap("<leader>ts", ":lua require('neotest').run.run()<cr>", { silent = true }, "Run the closest test")
+m.nmap("<leader>ta", ":lua require('neotest').run.run(vim.fn.expand('%'))<cr>", { silent = true }, "Run all tests")
+m.nmap("<leader>ds", ":lua require('neotest').run.run({ strategy = 'dap' })<cr>", { silent = true },
+  "Debug the closest test")
+m.nmap("<leader>T", ":lua require('neotest').summary.toggle()<cr>", { silent = true }, "Toggle the test summary view")
 
 neotest.setup({
   adapters = {
