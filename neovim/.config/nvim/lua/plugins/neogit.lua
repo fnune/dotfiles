10b1da7e586eb7de3ea3bfbed8daca2de11cae13
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-02 16:31:16 +0200

    Avoid persisting dangerous flags in neogit (neogit/issues/497)

--- neovim/.config/nvim/lua/plugins/neogit.lua
+++ neovim/.config/nvim/lua/plugins/neogit.lua
@@ -5,6 +5,12 @@ neogit.setup {
   disable_builtin_notifications = true,
   integrations = { diffview = true },
   kind = "vsplit",
+  -- These could become defaults https://github.com/TimUntersberger/neogit/issues/497
+  ignored_settings = {
+    "NeogitPushPopup--force-with-lease",
+    "NeogitPushPopup--force",
+    "NeogitCommitPopup--allow-empty",
+  },
 }
 
 nmap('<leader>g', '<cmd>Neogit<cr>')
