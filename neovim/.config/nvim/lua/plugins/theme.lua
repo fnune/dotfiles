4863d40d13dc85d54598507cd1865adab315902a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-06 14:56:08 +0200

    Disable italics in nvim

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,9 @@
 return {
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    "rose-pine/neovim",
+    name = "rose-pine",
     priority = 1000,
+    opts = { disable_italics = true },
     init = function()
       vim.cmd([[colorscheme rose-pine]])
     end
