9b2d60e0d76ac460d3a026032890f0e0ec3657e6
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-06 14:25:00 +0200

    Move back to rose-pine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,36 +1,22 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "Mofiqul/vscode.nvim" },
-  opts = {
-  },
   config = function()
     local lualine = require("lualine")
-    local theme = require("lualine.themes.vscode")
-
     lualine.setup({
       options = {
         component_separators = { "│", "│" },
         globalstatus = true,
         section_separators = { "", "" },
-        theme = theme,
       },
       -- See defaults: https://github.com/nvim-lualine/lualine.nvim#default-configuration
       sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'g:coc_status', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "g:coc_status", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
       },
     })
   end,
-  init = function()
-    local colors = require("vscode.colors").get_colors()
-    local theme = require("lualine.themes.vscode")
-
-    theme.normal.a.bg = colors.vscMediumBlue
-    theme.normal.b.fg = colors.vscMediumBlue
-    theme.normal.c.bg = colors.vscBlack
-  end
 }
--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,10 @@
 return {
-  "Mofiqul/vscode.nvim",
-  priority = 1000,
-  config = function()
-    local colors = require('vscode.colors').get_colors()
-
-    require('vscode').setup({
-      disable_nvimtree_bg = true,
-      group_overrides = {
-        ["@comment"] = { fg = colors.vscGray, italic = true },
-        Comment = { fg = colors.vscGray, italic = true },
-      }
-    })
-
-    require('vscode').load()
-  end,
-  init = function()
-    vim.opt.termguicolors = true
-    vim.opt.background = 'dark'
-  end
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    init = function()
+      vim.cmd([[colorscheme rose-pine]])
+    end
+  }
 }
