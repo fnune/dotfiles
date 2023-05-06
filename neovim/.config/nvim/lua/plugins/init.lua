38f4b3e97eb568c3606abebeb9f1e95b088774d5
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-06 15:19:30 +0200

    Theme which-key a bit

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   "AndrewRadev/tagalong.vim",
   "antoinemadec/FixCursorHold.nvim",
   "christoomey/vim-tmux-navigator",
-  "folke/which-key.nvim",
+  { "folke/which-key.nvim",         opts = { window = { border = "single", padding = { 0, 0, 0, 0 } } } },
   "machakann/vim-swap",
   "matze/vim-move",
   "mbbill/undotree",
@@ -14,10 +14,15 @@ return {
   "tpope/vim-repeat",
   "tpope/vim-rhubarb",
   "tpope/vim-surround",
-  { "b0o/mapx.nvim",                priority = 51, opts = { whichkey = true } },
-  { "kevinhwang91/nvim-bqf",        lazy = true,   ft = "qf" },
-  { "kristijanhusak/vim-dadbod-ui", lazy = true,   cmd = "DBUI" },
+  {
+    "b0o/mapx.nvim",
+    priority = 51,
+    opts = {
+      whichkey = true }
+  },
+  { "kevinhwang91/nvim-bqf",        lazy = true,                                                        ft = "qf" },
+  { "kristijanhusak/vim-dadbod-ui", lazy = true,                                                        cmd = "DBUI" },
   { "lewis6991/gitsigns.nvim",      config = true },
-  { "nvim-lua/plenary.nvim",    priority = 51 },
-  { "tversteeg/registers.nvim", lazy = true },
+  { "nvim-lua/plenary.nvim",        priority = 51 },
+  { "tversteeg/registers.nvim",     lazy = true },
 }
