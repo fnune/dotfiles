8c51c1ae897595015df5594c5f2e42d22a4fda40
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-06 13:02:28 +0200

    Disable change detection notification in lazy.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -6,11 +6,16 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+local lazy = require("lazy")
+
+lazy.setup({
+  spec = { import = "plugins" },
+  change_detection = { enabled = true, notify = false },
+})
