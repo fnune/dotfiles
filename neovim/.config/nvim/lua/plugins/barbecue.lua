5044ec80b0961cfca4e0f28444b5ba9c74ea4d52
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-14 21:23:08 +0200

    Use barbecue instead of winbar

---
+++ neovim/.config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,9 @@
+return {
+  { "SmiteshP/nvim-navic" },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+}
