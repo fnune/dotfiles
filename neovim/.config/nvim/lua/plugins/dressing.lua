50afeb3f0e60e6d674f657764d85b3cbb085e5fd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-14 16:50:57 +0200

    Use dressing.nvim with telescope for code actions select

---
+++ neovim/.config/nvim/lua/plugins/dressing.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/dressing.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    select = {
+      get_config = function(opts)
+        if opts.kind == 'codeaction' then
+          return {
+            backend = 'telescope',
+            telescope = require("telescope.themes").get_cursor(),
+          }
+        end
+      end
+    },
+  },
+}
--- neovim/.config/nvim/lua/plugins/popui.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "hood/popui.nvim",
-  config = function()
-    vim.ui.select = require("popui.ui-overrider")
-    vim.g.popui_border_style = "rounded"
-  end,
-}
