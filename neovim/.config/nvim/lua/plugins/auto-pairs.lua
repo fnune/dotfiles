123d1bd32399665a6b039e890fe764460277feac
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-21 11:50:08 +0200

    Remove nvim-autopairs + nvim-cmp integration: it breaks jsx

--- neovim/.config/nvim/lua/plugins/auto-pairs.lua
+++ neovim/.config/nvim/lua/plugins/auto-pairs.lua
@@ -1,14 +1,5 @@
 return {
   "windwp/nvim-autopairs",
   dependencies = { "hrsh7th/nvim-cmp" },
-  config = function()
-    local autopairs = require("nvim-autopairs")
-
-    autopairs.setup({})
-
-    local cmp = require("cmp")
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end
+  opts = {},
 }
