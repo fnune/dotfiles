30acfcd15efa64111df813ccf6bcfb482076a591
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-04 19:58:47 +0200

    Try neovim-session-manager

---
+++ neovim/.config/nvim/lua/plugins/session-manager.lua
@@ -0,0 +1,21 @@
+return {
+  "Shatur/neovim-session-manager",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local config = require("session_manager.config")
+    local session_manager = require("session_manager")
+
+    session_manager.setup({
+      sessions_dir = require("plenary.path"):new(vim.fn.stdpath("data"), "sessions"),
+      autoload_mode = config.AutoloadMode.CurrentDir,
+      autosave_last_session = true,
+      autosave_ignore_not_normal = true,
+      autosave_ignore_filetypes = {
+        "gitcommit",
+        "gitrebase",
+      },
+      autosave_ignore_buftypes = {},
+      max_path_length = 80,
+    })
+  end
+}
