654ffc5500e6006f953723e1069e2317a4954329
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-04 19:34:58 +0200

    Add wilder.nvim

---
+++ neovim/.config/nvim/lua/plugins/wilder.lua
@@ -0,0 +1,17 @@
+return {
+  "gelguy/wilder.nvim",
+  config = function()
+    local constants = require("constants")
+    local wilder = require("wilder")
+
+    wilder.setup({
+      modes = { ':', '/', '?' },
+    })
+
+    wilder.set_option('renderer', wilder.popupmenu_renderer(
+      wilder.popupmenu_border_theme({
+        border = constants.floating_border,
+      })
+    ))
+  end,
+}
