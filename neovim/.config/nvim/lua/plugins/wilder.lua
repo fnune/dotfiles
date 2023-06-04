a40143fdb4ca07fd73fb57607008e4f7e3b65052
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-04 19:35:35 +0200

    Remove ? and / from wilder modes

--- neovim/.config/nvim/lua/plugins/wilder.lua
+++ neovim/.config/nvim/lua/plugins/wilder.lua
@@ -4,10 +4,7 @@ return {
     local constants = require("constants")
     local wilder = require("wilder")
 
-    wilder.setup({
-      modes = { ':', '/', '?' },
-    })
-
+    wilder.setup({ modes = { ':' } })
     wilder.set_option('renderer', wilder.popupmenu_renderer(
       wilder.popupmenu_border_theme({
         border = constants.floating_border,
