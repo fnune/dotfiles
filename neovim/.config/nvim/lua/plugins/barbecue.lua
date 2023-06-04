e1fbd9d18cae9a61a5bc564eda3ac44c53878206
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-04 18:45:35 +0200

    Remove barbecue

--- neovim/.config/nvim/lua/plugins/barbecue.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "SmiteshP/nvim-navic" },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
-    opts = {},
-  },
-}
