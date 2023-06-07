1129523a3b2ab9d730ba5becf7dbbb09997c694e
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-07 20:33:13 +0200

    Use italics for more things

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,4 +1,5 @@
 return {
+  { "nvim-treesitter/playground" },
   {
     "rose-pine/neovim",
     name = "rose-pine",
@@ -7,6 +8,10 @@ return {
     init = function()
       vim.cmd([[colorscheme rose-pine]])
       vim.cmd([[highlight Comment cterm=italic gui=italic]])
+      vim.cmd([[highlight Keyword cterm=italic gui=italic]])
+      vim.cmd([[highlight Conditional cterm=italic gui=italic]])
+      vim.cmd([[highlight Repeat cterm=italic gui=italic]])
+      vim.cmd([[highlight Include cterm=italic gui=italic]])
       vim.cmd([[hi! link EndOfBuffer BufferOffset]])
     end
   }
