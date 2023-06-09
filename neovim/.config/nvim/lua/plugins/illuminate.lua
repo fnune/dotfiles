680f1f7cbe8491cb169844bba9d28489613efe4a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-09 09:16:38 +0200

    Exclude some filetypes from illuminate

--- neovim/.config/nvim/lua/plugins/illuminate.lua
+++ neovim/.config/nvim/lua/plugins/illuminate.lua
@@ -2,7 +2,15 @@ return {
   "RRethy/vim-illuminate",
   config = function()
     local illuminate = require("illuminate")
-    illuminate.configure({ delay = 300 })
+    illuminate.configure({
+      delay = 300,
+      filetypes_denylist = {
+        "NvimTree",
+        "NeogitStatus",
+        "NeogitPopup",
+        "aerial",
+      },
+    })
 
     vim.cmd([[
       hi! link IlluminatedWordText Visual
