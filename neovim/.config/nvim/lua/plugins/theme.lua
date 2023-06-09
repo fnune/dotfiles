13b18125c7beb4eff555462febedccfed9cd7e93
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-09 11:14:35 +0200

    Organize italics code and add rule for @keyword.operator

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -8,12 +8,16 @@ return {
     init = function()
       vim.opt.termguicolors = true
       vim.cmd([[colorscheme rose-pine-moon]])
+
+      vim.cmd([[hi! link EndOfBuffer BufferOffset]]) -- Hide ~ end-of-buffer characters
+
+      -- Manually control italics:
+      vim.cmd([[highlight @keyword.operator cterm=italic gui=italic]])
       vim.cmd([[highlight Comment cterm=italic gui=italic]])
-      vim.cmd([[highlight Keyword cterm=italic gui=italic]])
       vim.cmd([[highlight Conditional cterm=italic gui=italic]])
-      vim.cmd([[highlight Repeat cterm=italic gui=italic]])
       vim.cmd([[highlight Include cterm=italic gui=italic]])
-      vim.cmd([[hi! link EndOfBuffer BufferOffset]])
+      vim.cmd([[highlight Keyword cterm=italic gui=italic]])
+      vim.cmd([[highlight Repeat cterm=italic gui=italic]])
     end
   },
 }
