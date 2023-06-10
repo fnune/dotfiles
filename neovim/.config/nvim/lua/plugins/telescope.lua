96689084953880f1cb101a1bfcd165fae3ee96ae
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-10 17:57:34 +0200

    Try smart-open.nvim as a file finder

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -11,11 +11,12 @@ return {
         live_grep = { additional_args = { "--hidden" } },
         colorscheme = { enable_preview = true },
       },
+      extensions = {
+        smart_open = { match_algorithm = "fzf" },
+      },
     },
     init = function()
       local m = require("mapx")
-
-      m.nmap("<leader>f", ":Telescope find_files theme=ivy<CR>", { silent = true }, "Find files")
       m.nmap("<leader>F", ":Telescope live_grep theme=ivy<CR>", { silent = true }, "Find text in files")
     end,
   },
@@ -25,8 +26,24 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim" },
     init = function()
       local telescope = require("telescope")
-
       telescope.load_extension("fzf")
     end
+  },
+  {
+    "danielfalk/smart-open.nvim",
+    branch = "0.2.x",
+    dependencies = {
+      "kkharji/sqlite.lua",
+      "nvim-telescope/telescope.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.load_extension("smart_open")
+    end,
+    init = function()
+      local m = require("mapx")
+      m.nmap("<leader>f", ":Telescope smart_open theme=ivy<CR>", { silent = true }, "Find files")
+    end,
   },
 }
