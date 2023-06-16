1bf04968595999c37b89a9002487d14567af8edd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-16 16:37:03 +0200

    Put theming things not related to rose-pine outside its setup

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,3 +1,18 @@
+local function set_colorscheme(colorscheme)
+  vim.opt.termguicolors = true
+  vim.cmd.colorscheme(colorscheme)
+
+  vim.cmd([[hi! link EndOfBuffer BufferOffset]]) -- Hide ~ end-of-buffer characters
+
+  -- Manually control italics:
+  vim.cmd([[highlight @keyword.operator cterm=italic gui=italic]])
+  vim.cmd([[highlight Comment cterm=italic gui=italic]])
+  vim.cmd([[highlight Conditional cterm=italic gui=italic]])
+  vim.cmd([[highlight Include cterm=italic gui=italic]])
+  vim.cmd([[highlight Keyword cterm=italic gui=italic]])
+  vim.cmd([[highlight Repeat cterm=italic gui=italic]])
+end
+
 return {
   { "nvim-treesitter/playground" },
   {
@@ -5,19 +20,9 @@ return {
     name = "rose-pine",
     priority = 1000,
     opts = { disable_italics = true, disable_float_background = true },
+    enabled = true,
     init = function()
-      vim.opt.termguicolors = true
-      vim.cmd([[colorscheme rose-pine-moon]])
-
-      vim.cmd([[hi! link EndOfBuffer BufferOffset]]) -- Hide ~ end-of-buffer characters
-
-      -- Manually control italics:
-      vim.cmd([[highlight @keyword.operator cterm=italic gui=italic]])
-      vim.cmd([[highlight Comment cterm=italic gui=italic]])
-      vim.cmd([[highlight Conditional cterm=italic gui=italic]])
-      vim.cmd([[highlight Include cterm=italic gui=italic]])
-      vim.cmd([[highlight Keyword cterm=italic gui=italic]])
-      vim.cmd([[highlight Repeat cterm=italic gui=italic]])
+      set_colorscheme("rose-pine-moon")
     end
   },
 }
