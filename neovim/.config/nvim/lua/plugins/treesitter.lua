fdaa6bd23570bf8adb718108c18628a06fd98fd2
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-06-20 10:01:20 +0200

    Add explicit list of ensure_installed for treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,17 @@ return {
       local treesitter_configs = require("nvim-treesitter.configs")
 
       treesitter_configs.setup {
-        ensure_installed = "all",
+        ensure_installed = { "arduino", "awk", "bash", "bibtex", "c", "c_sharp", "clojure", "cmake", "comment",
+          "commonlisp", "cpp", "css", "dart", "devicetree", "diff", "dockerfile", "dot", "eex", "elixir", "elm",
+          "erlang", "fish", "fortran", "gdscript", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+          "go", "godot_resource", "gomod", "gosum", "gowork", "graphql", "haskell", "hcl", "html", "htmldjango", "http",
+          "ini", "java", "javascript", "jq", "jsdoc", "json", "json5", "jsonc", "julia", "kdl", "kotlin", "latex",
+          "llvm", "lua", "luadoc", "luap", "luau", "make", "markdown", "markdown_inline", "matlab", "mermaid", "meson",
+          "ninja", "nix", "norg", "objc", "ocaml", "ocaml_interface", "org", "pascal", "passwd", "perl", "php", "phpdoc",
+          "prisma", "proto", "pug", "python", "query", "r", "racket", "regex", "rst", "ruby", "rust", "scala", "scheme",
+          "scss", "sparql", "sql", "svelte", "sxhkdrc", "terraform", "tlaplus", "todotxt", "toml", "tsx", "typescript",
+          "vala", "vim", "vimdoc", "vue", "yaml", "zig",
+        },
         sync_install = false,
         auto_install = true,
         highlight = { enable = true },
