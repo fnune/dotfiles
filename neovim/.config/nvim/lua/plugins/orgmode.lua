c2e8eb01f0a3e7ebb90bd5f3c14e2c615fb1a3aa
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-07-02 23:37:36 +0200

    Further configure orgmode.nvim

--- neovim/.config/nvim/lua/plugins/orgmode.lua
+++ neovim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,3 +1,4 @@
+local constants = require("constants")
 return {
   "nvim-orgmode/orgmode",
   config = function()
@@ -8,7 +9,10 @@ return {
     orgmode.setup({
       org_agenda_files = { "~/.journal/org/*" },
       org_default_notes_file = "~/.journal/org/refile.org",
+      org_log_into_drawer = "LOGBOOK",
       org_todo_keywords = { "TODO(t)", "|", "DONE(d)" },
+      win_border = constants.floating_border,
+      win_split_mode = "float",
     })
   end
 }
