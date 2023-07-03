e9c77faa6404f11ab83ebd2f39dda01103a0aa4d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-07-03 16:26:55 +0200

    Remove mapx from top-level mappings which broke first-time installs

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -1,13 +1,22 @@
-local m = require("mapx")
+-- Move to the first non-blank character of the line
+vim.api.nvim_set_keymap("n", "0", "^", { noremap = true })
 
-m.nnoremap("0", "^")
-m.nnoremap("Q", "<nop>")
-m.nmap("j", "gj")
-m.nmap("k", "gk")
+-- Disable the Q command
+vim.api.nvim_set_keymap("n", "Q", "<nop>", { noremap = true })
 
-m.nmap("<C-u>", "<C-u>zz", "Center the buffer after scrolling")
-m.nmap("<C-d>", "<C-d>zz", "Center the buffer after scrolling")
+-- Move by display lines, not actual lines
+vim.api.nvim_set_keymap("n", "j", "gj", {})
+vim.api.nvim_set_keymap("n", "k", "gk", {})
 
-m.nmap("gp", "`[v`]", "Select last pasted text")
-m.nmap("<leader>l", "<cmd>nohlsearch<cr>", "Clear hlsearch highlights")
-m.tmap("<C-t><esc>", "<C-\\><C-n>", "Exit insert mode in the terminal")
+-- Center the buffer after scrolling up or down
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", {})
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", {})
+
+-- Select last pasted text
+vim.api.nvim_set_keymap("n", "gp", "`[v`]", {})
+
+-- Clear hlsearch highlights
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>nohlsearch<cr>", {})
+
+-- Exit insert mode in the terminal
+vim.api.nvim_set_keymap("t", "<C-t><esc>", "<C-\\><C-n>", {})
