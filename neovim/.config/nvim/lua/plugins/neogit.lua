2c7d4afac5a4e353958a82951896c899d7d422b0
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-08-04 17:07:37 +0200

    Add highlights to match vscode theme

--- neovim/.config/nvim/lua/plugins/neogit.lua
+++ neovim/.config/nvim/lua/plugins/neogit.lua
@@ -11,5 +11,21 @@ return {
     local m = require("mapx")
 
     m.nmap("<leader>g", ":Neogit<cr>", { silent = true }, "Open neogit")
+
+    local colors = require("vscode.colors").get_colors()
+
+    -- Signs for line highlighting
+    vim.cmd("hi def NeogitHunkHeader guibg=" .. colors.vscTabOutside .. " guifg=" .. colors.vscPopupFront)
+    vim.cmd("hi def NeogitDiffContext guibg=" .. colors.vscLeftDark .. " guifg=" .. colors.vscPopupFront)
+    vim.cmd("hi def NeogitDiffAdd guibg=" .. colors.vscDiffGreenDark .. " guifg=" .. colors.vscGitAdded)
+    vim.cmd("hi def NeogitDiffDelete guibg=" .. colors.vscDiffRedDark .. " guifg=" .. colors.vscGitDeleted)
+    vim.cmd("hi def NeogitDiffHeader guibg=" .. colors.vscContext .. " guifg=" .. colors.vscSplitLight)
+
+    -- Signs for line highlighting current context
+    vim.cmd("hi def NeogitHunkHeaderHighlight guibg=" .. colors.vscTabOutside .. " guifg=" .. colors.vscPopupFront)
+    vim.cmd("hi def NeogitDiffContextHighlight guibg=" .. colors.vscLeftMid .. " guifg=" .. colors.vscPopupFront)
+    vim.cmd("hi def NeogitDiffAddHighlight guibg=" .. colors.vscDiffGreenLight .. " guifg=" .. colors.vscGitAdded)
+    vim.cmd("hi def NeogitDiffDeleteHighlight guibg=" .. colors.vscDiffRedLight .. " guifg=" .. colors.vscGitDeleted)
+    vim.cmd("hi def NeogitDiffHeaderHighlight guibg=" .. colors.vscContext .. " guifg=" .. colors.vscSplitLight)
   end
 }
